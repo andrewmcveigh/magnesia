9cfa558a0aa2fb1c6e661f17e2213348118eecb4
Andrew Mcveigh <me@andrewmcveigh.com>
2017-01-09 21:46:09 +0000

    read_string, read_keyword, read_regex

--- src/reader.rs
+++ src/reader.rs
@@ -1,15 +1,18 @@
-use std::vec::*;
-use std::str::Chars;
+use std::fmt;
 use std::iter::Peekable;
+use std::str::Chars;
+use std::vec::*;
 use types::*;
 // use std::result::*;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Eq)]
 enum ReaderError {
     EOF,
     CannotUnreadChar,
     InvalidToken,
-    InvalidSymbol
+    InvalidCharacter,
+    InvalidSymbol,
+    InvalidKeyword
 }
 
 struct Reader<'a> {
@@ -22,10 +25,6 @@ type ReaderResult<T> = Result<T, ReaderError>;
 fn string_reader(s : &str) -> Reader {
     let s = s.clone();
     Reader { s:  s.chars().peekable(), buf: Vec::new() }
-}
-
-fn reader(s : Peekable<Chars>, buf : Vec<char>) -> Reader {
-    Reader { s: s, buf: buf }
 }
 
 fn peek_char(r : &mut Reader) -> ReaderResult<char> {
@@ -53,7 +52,7 @@ fn unread_char(r : &mut Reader, c : char) -> ReaderResult<()> {
     Ok(r.buf.push(c))
 }
 
-fn read_while(r : &mut Reader, p : &Fn(char) -> bool) -> ReaderResult<String> {
+fn read_while(r : &mut Reader, p : &Fn(char) -> bool, eof_err : bool) -> ReaderResult<String> {
     let mut s = "".to_string();
     while let Ok(c) = read_char(r) {
         if p(c) {
@@ -62,8 +61,8 @@ fn read_while(r : &mut Reader, p : &Fn(char) -> bool) -> ReaderResult<String> {
         } else {
             s.push(c)
         }
-    }
-    Err(ReaderError::EOF)
+    };
+    if eof_err { Err(ReaderError::EOF) } else { Ok(s) }
 }
 
 fn is_macro_terminating(c : char) -> bool {
@@ -81,8 +80,39 @@ fn is_whitespace(c : char) -> bool {
     }
 }
 
+fn escape_char<'a>(c : char) -> ReaderResult<&'a str> {
+    match c {
+        't'  => Ok("\t"),
+        'r'  => Ok("\r"),
+        'n'  => Ok("\n"),
+        '\\' => Ok("\\"),
+        '"'  => Ok("\""),
+        _    => Err(ReaderError::InvalidCharacter)
+    }
+}
+
+fn read_string_type(r : &mut Reader, _ : char) -> ReaderResult<String> {
+    let mut s = "".to_string();
+    loop {
+        let c = read_char(r);
+        match c {
+            Ok('"') => { return Ok(s) },
+            Ok('\\') => try!(read_char(r)
+                             .and_then(escape_char)
+                             .map(|c| s.push_str(c))),
+            Ok(c) => s.push(c),
+            Err(e) => return Err(e)
+        }
+    };
+    Ok(s)
+}
+
+fn read_regex(r : &mut Reader, _ : char) -> ReaderResult<Pattern> {
+    read_while(r, &|c| c == '"', true).map(|s| Pattern(s))
+}
+
 fn read_token(r : &mut Reader, initch : char) -> ReaderResult<String> {
-    read_while(r, &|c| is_macro_terminating(c) || is_whitespace(c))
+    read_while(r, &|c| is_macro_terminating(c) || is_whitespace(c), false)
         .map(|s| initch.to_string() + &s)
 }
 
@@ -113,34 +143,21 @@ fn read_symbol(r : &mut Reader, initch : char) -> ReaderResult<Symbol> {
     }
 }
 
-// (defn- read-keyword
-//   [reader initch opts pending-forms]
-//   (let [ch (read-char reader)]
-//     (if-not (whitespace? ch)
-//       (let [token (read-token reader ch)
-//             s (parse-symbol token)]
-//         (if s
-//           (let [^String ns (s 0)
-//                 ^String name (s 1)]
-//             (if (identical? \: (nth token 0))
-//               (if ns
-//                 (let [ns (resolve-ns (symbol (subs ns 1)))]
-//                   (if ns
-//                     (keyword (str ns) name)
-//                     (reader-error reader "Invalid token: :" token)))
-//                 (keyword (str *ns*) (subs name 1)))
-//               (keyword ns name)))
-//           (reader-error reader "Invalid token: :" token)))
-// (reader-error reader "Invalid token: :"))))
-
-fn read_keyword(r : &mut Reader) -> ReaderResult<Symbol> {
+fn read_keyword(r : &mut Reader, _ : char) -> ReaderResult<Keyword> {
     match read_char(r) {
         Ok(c) if is_whitespace(c) => Err(ReaderError::InvalidToken),
-        Ok(c) => let token = read_token(
-    match read_token(r).and_then(parse_symbol) {
-        Ok((None,     name)) => Ok(Symbol::SimpleSymbol(name)),
-        Ok((Some(ns), name)) => Ok(Symbol::NamespacedSymbol(ns, name)),
-        Err(e)               => Err(e)
+        Ok(c) => {
+            let token = try!(read_token(r, c));
+            let (ns, name) = try!(parse_symbol(token.clone()));
+            match token.chars().nth(0) {
+                Some(':') => Err(ReaderError::InvalidKeyword),
+                _ => match ns {
+                    Some(ns) => Ok(Keyword::NamespacedKeyword(ns, name)),
+                    None => Ok(Keyword::SimpleKeyword(name))
+                }
+            }
+        },
+        Err(e) => Err(e)
     }
 }
 
@@ -169,13 +186,33 @@ fn unread_char_test() {
 fn read_while_test() {
     let mut r = string_reader("abc ");
     let f = & |c| c == ' ';
-    assert!(read_while(&mut r, f).expect("Failed read_while") == "abc");
+    assert!(read_while(&mut r, f, false).expect("Failed read_while") == "abc");
     let mut r2 = string_reader("abc");
-    match read_while(&mut r2, f) {
+    match read_while(&mut r2, f, true) {
         Ok(_) => panic!("Shouldn't have succeeded read_while"),
         Err(ReaderError::EOF) => (),
         _ => panic!("Should have Err(ReaderError::EOF)")
     }
+}
+
+#[test]
+fn read_string_type_test() {
+    let mut r = string_reader("abc\"");
+    assert!(read_string_type(&mut r, '"') == Ok("abc".to_string()));
+    let mut r = string_reader("abc\\\\\"");
+    assert!(read_string_type(&mut r, '"') == Ok("abc\\".to_string()));
+    let mut r = string_reader("abc");
+    assert!(read_string_type(&mut r, '"') == Err(ReaderError::EOF));
+}
+
+#[test]
+fn read_regex_test() {
+    let mut r = string_reader("abc\"");
+    assert!(read_regex(&mut r, '"') == Ok(Pattern("abc".to_string())));
+    let mut r = string_reader("abc\\\"");
+    assert!(read_regex(&mut r, '"') == Ok(Pattern("abc\\".to_string())));
+    let mut r = string_reader("abc");
+    assert!(read_regex(&mut r, '"') == Err(ReaderError::EOF));
 }
 
 #[test]
@@ -189,6 +226,14 @@ fn read_token_test() {
 }
 
 #[test]
+fn parse_symbol_test() {
+    assert!(parse_symbol("abc".to_string()) == Ok((None, "abc".to_string())));
+    assert!(parse_symbol(":a".to_string()) == Ok((None, ":a".to_string())));
+    assert!(parse_symbol(":".to_string()) == Err(ReaderError::InvalidSymbol));
+    assert!(parse_symbol(":a".to_string()) == Ok((None, ":a".to_string())));
+}
+
+#[test]
 fn read_symbol_test() {
     let mut r = string_reader("bc ");
     let sym = read_symbol(&mut r, 'a').expect("Failed read_token");
@@ -199,4 +244,24 @@ fn read_symbol_test() {
     let mut r3 = string_reader(" ");
     let sym3 = read_symbol(&mut r3, '/').expect("Failed read_token");
     assert!(sym3 == Symbol::SimpleSymbol("/".to_string()));
+}
+
+#[test]
+fn read_keyword_test() {
+    let mut r = string_reader("abc ");
+    let key = read_keyword(&mut r, ':').expect("Failed read_token");
+    assert!(key == Keyword::SimpleKeyword("abc".to_string()));
+    let mut r2 = string_reader("ns1/abc ");
+    let key = read_keyword(&mut r2, ':').expect("Failed read_token");
+    assert!(key == Keyword::NamespacedKeyword("ns1".to_string(), "abc".to_string()));
+    let mut r3 = string_reader(" ");
+    match read_keyword(&mut r3, ':') {
+        Err(err) => assert!(err == ReaderError::InvalidToken),
+        _ => panic!("Should have ReaderError::InvalidToken")
+    }
+    let mut r3 = string_reader(":a ");
+    match read_keyword(&mut r3, ':') {
+        Err(err) => assert!(err == ReaderError::InvalidKeyword),
+        _ => panic!("Should have ReaderError::InvalidKeyword")
+    }
 }
--- src/types.rs
+++ src/types.rs
@@ -45,3 +45,12 @@ impl fmt::Display for Keyword {
         }
     }
 }
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct Pattern(pub String);
+
+impl fmt::Display for Pattern {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "#\"{}\"", self.0)
+    }
+}
